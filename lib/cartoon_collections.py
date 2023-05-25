a48a96169d93bba5f4e51a25b347974e06ce6208
Christian Garcia <bones192nd@gmail.com>
2023-05-25 14:42:14 -0400

    lab submission

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,23 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    for index, dwarf in enumerate(dwarfs):
+        print("{}. {}".format(index + 1, dwarf))
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(values):
+    return [value.capitalize() + "!" for value in values]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    status = False
+    for word in words:
+        if len(word) > 4:
+            status = True
+            break
+    return status
 
-def find_the_cheese():
-    pass
+def find_the_cheese(cheeses):
+    cheese_set = set(cheeses)
+    cheese_check = {"cheddar", "gouda", "camembert"}
+    answer = cheese_set & cheese_check
+    try:
+        return answer.pop()
+    except:
+        return None
